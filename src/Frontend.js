126b44e1d9c7d7594cb174146d7642034a3e0f19
Aaryan <aaryanrajsarda@gmail.com>
2021-04-13 19:01:34 +0530

    Frontend Section Added

---
+++ src/Frontend.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+function Frontend() {
+  return (
+    <div
+      className="container-fluid"
+      style={{
+        backgroundColor: "#FFFBD4",
+      }}
+    >
+      <div
+        className="row"
+        style={{ paddingBottom: "2rem", paddingTop: "3rem" }}
+      >
+        <div className="col-12">
+          <center>
+            <h2>
+              <u>
+                <strong>Frontend</strong>
+              </u>
+            </h2>
+
+            <h3 className="tagline">
+              Courses to teach you how to build apps in the browser
+            </h3>
+            <button
+              type="button"
+              class="see-all"
+              style={{
+                marginTop: "5rem",
+              }}
+            >
+              <strong>See All</strong>
+            </button>
+          </center>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Frontend;
