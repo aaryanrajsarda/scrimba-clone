ca622b13c02ef8d121a6704c8f12cd28a74de8a7
Aaryan <aaryanrajsarda@gmail.com>
2021-04-15 00:35:11 +0530

    Updated Header

--- src/Header.js
+++ src/Header.js
@@ -1,16 +1,28 @@
 import React from "react";
+import "./header.css";
 
 function Header() {
   return (
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar fixed-top navbar-expand-lg navbar-light navigate">
       <span class="navbar-brand">
         <img src="dark-logo.svg" height="20" alt="scrimba-logo" />
       </span>
       <div class="collapse navbar-collapse">
-        <div class="navbar-nav">
-          <span class="nav-item">All Courses</span>
-          <span class="nav-item">Sign In</span>
-          <span class="nav-item disabled">Sign Up</span>
+        <div class="navbar-nav ml-auto">
+          <div className="d-none d-sm-block">
+            <button type="button" class="nav-item btn go-pro">
+              Go Pro!
+            </button>
+          </div>
+          <div>
+            <h5 class="nav-item px-2">All Courses</h5>
+          </div>
+          <div>
+            <h5 class="nav-item px-2">Sign In</h5>
+          </div>
+          <div className="d-none d-md-block">
+            <h5 class="nav-item px-2">Sign Up</h5>
+          </div>
         </div>
       </div>
     </nav>
