15dd7305f9cc2982d9e62e9f8efef71c78fc6b6b
Aaryan <aaryanrajsarda@gmail.com>
2021-04-15 01:50:09 +0530

    Header Scroll Shadow Added

--- src/Header.js
+++ src/Header.js
@@ -1,27 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import "./header.css";
+import { useScrollPosition } from "@n8tb1t/use-scroll-position";
 
 function Header() {
-  return (
-    <nav class="navbar fixed-top navbar-expand-lg navbar-light navigate">
-      <span class="navbar-brand">
+  const [headerStyle, setHeaderStyle] = useState("scrollInvisible");
+
+  useScrollPosition(
+    ({ prevPos, currPos }) => {
+      const isInvisible = currPos.y > -15;
+      console.log(currPos.y);
+      const shouldBeStyle = isInvisible ? "scrollInvisible" : "scrollVisible";
+      setHeaderStyle(shouldBeStyle);
+    },
+    [headerStyle]
+  );
+  return headerStyle === "scrollInvisible" ? (
+    <nav className="navbar fixed-top navbar-expand-lg navbar-light navigate">
+      <span className="navbar-brand">
         <img src="dark-logo.svg" height="20" alt="scrimba-logo" />
       </span>
-      <div class="collapse navbar-collapse">
-        <div class="navbar-nav ml-auto">
+      <div className="collapse navbar-collapse">
+        <div className="navbar-nav ml-auto">
           <div className="d-none d-sm-block">
-            <button type="button" class="nav-item btn go-pro">
+            <button type="button" className="nav-item btn go-pro">
               Go Pro!
             </button>
           </div>
           <div className="item">
-            <h5 class="nav-item px-2">All Courses</h5>
+            <h5 className="nav-item px-2">All Courses</h5>
           </div>
           <div className="item">
-            <h5 class="nav-item px-2">Sign In</h5>
+            <h5 className="nav-item px-2">Sign In</h5>
           </div>
           <div className="item d-none d-md-block">
-            <h5 class="nav-item px-2">Sign Up</h5>
+            <h5 className="nav-item px-2">Sign Up</h5>
+          </div>
+        </div>
+      </div>
+    </nav>
+  ) : (
+    <nav className="navbar fixed-top navbar-expand-lg navbar-light navigate scrollVisible">
+      <span className="navbar-brand">
+        <img src="dark-logo.svg" height="20" alt="scrimba-logo" />
+      </span>
+      <div className="collapse navbar-collapse">
+        <div className="navbar-nav ml-auto">
+          <div className="d-none d-sm-block">
+            <button type="button" className="nav-item btn go-pro">
+              Go Pro!
+            </button>
+          </div>
+          <div className="item">
+            <h5 className="nav-item px-2">All Courses</h5>
+          </div>
+          <div className="item">
+            <h5 className="nav-item px-2">Sign In</h5>
+          </div>
+          <div className="item d-none d-md-block">
+            <h5 className="nav-item px-2">Sign Up</h5>
           </div>
         </div>
       </div>
