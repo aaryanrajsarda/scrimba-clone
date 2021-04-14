c7b031164c0131d578b57c610c0bf85a04b3c0a7
Aaryan <aaryanrajsarda@gmail.com>
2021-04-14 23:29:35 +0530

    Jumbotron Set Up

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Frontend from "./Frontend";
 import PopularCourses from "./PopularCourses";
+import MainJumbotron from "./MainJumbotron";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MainJumbotron />
       <PopularCourses />
       <Frontend />
       <Footer />
