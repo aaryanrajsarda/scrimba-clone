dac378578c16e1b17313bbed2934646babbb0976
Aaryan <aaryanrajsarda@gmail.com>
2021-04-17 01:41:04 +0530

    Added Basic Layout

--- src/JavaScript.js
+++ src/JavaScript.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./javascript.css";
 
 function JavaScript() {
   return (
@@ -8,31 +9,419 @@ function JavaScript() {
         backgroundColor: "#DDEAE1",
       }}
     >
-      <div
-        className="row"
-        style={{ paddingBottom: "2rem", paddingTop: "3rem" }}
-      >
-        <div className="col-12">
-          <center>
-            <h2>
-              <u>
-                <strong>JavaScript</strong>
-              </u>
-            </h2>
-
-            <h3 className="tagline">
-              JavaScript is the worlds most popular programming language
-            </h3>
-            <button
-              type="button"
-              class="see-all"
+      <div className="row" style={{ padding: "3rem 9rem 2rem 9rem" }}>
+        <div className="col-12 pb-3">
+          <h2>
+            <u>
+              <strong>JavaScript</strong>
+            </u>
+          </h2>
+          <h3 className="tagline">
+            JavaScript is the worlds most popular programming language
+          </h3>
+        </div>
+        <div className="col-12 col-md-6 col-lg-3">
+          <div className="card course-card">
+            <div
               style={{
-                marginTop: "5rem",
+                backgroundColor: "#FFFBD6",
+                padding: "1rem",
+                display: "grid",
+                zIndex: "0",
+                placeItems: "center",
+                position: "relative",
+                height: "60%",
               }}
             >
-              <strong>See All</strong>
-            </button>
-          </center>
+              <div className="shapes">
+                <svg
+                  class="bootcamp-card1-shape1 img-fluid"
+                  width="100"
+                  viewBox="0 0 100 100"
+                  preserveAspectRatio="none"
+                  height="100"
+                >
+                  <path
+                    style={{ fill: "hsla(34.00,95.75%,68.50%,100%)" }}
+                    d="M48.3671 4.00414C61.7855 2.7251 76.745 -1.97133 86.4625 9.80638C97.2181 22.8423 100.354 43.9553 96.2873 62.0843C92.4299 79.2819 80.463 91.9101 66.9118 96.9691C55.504 101.228 45.1731 91.3023 34.2907 85.2291C22.5778 78.6925 6.96447 76.7547 3.05352 61.3315C-1.02481 45.2483 7.40784 28.3849 16.8479 16.442C25.0359 6.08323 36.8209 5.10471 48.3671 4.00414Z"
+                  ></path>
+                </svg>
+                <svg
+                  class="bootcamp-card1-shape2 img-fluid"
+                  width="100"
+                  viewBox="0 0 100 100"
+                  preserveAspectRatio="none"
+                  height="100"
+                >
+                  <path
+                    style={{ fill: "hsla(220.00,100.00%,91.00%,100%)" }}
+                    d="M46.6443 2.02753C66.6399 1.53324 88.5794 7.59278 95.422 28.5087C102.874 51.2865 93.5364 76.7516 75.5776 90.4677C58.9651 103.156 37.2191 98.7293 21.1053 85.2685C5.82684 72.5056 -2.06419 50.8861 4.10887 30.7659C10.0345 11.4522 28.3111 2.48073 46.6443 2.02753Z"
+                  ></path>
+                </svg>
+              </div>
+              <h2 className="text-right course-name">
+                The Frontend Developer Career Path
+              </h2>
+            </div>
+            <div className="card-body p-2">
+              <div className="row">
+                <div className="col-auto">
+                  <p
+                    className="card-text text-left"
+                    style={{
+                      fontSize: "12px",
+                      fontWeight: "500",
+                    }}
+                  >
+                    928 lessons | 75 hours
+                  </p>
+                  <p
+                    style={{
+                      fontSize: "12px",
+                      fontWeight: "500",
+                      marginTop: "-1rem",
+                    }}
+                    className="card-text text-left"
+                  >
+                    with <strong>Per Harald Borgen</strong>
+                  </p>
+                </div>
+                <div className="col-auto ml-auto">
+                  <img
+                    src="https://scrimba.com/avatars/perborgen/64"
+                    className="img-fluid avatar rounded-circle"
+                  />
+                </div>
+              </div>
+              <div className="row pt-5">
+                <div className="col">
+                  <p
+                    style={{
+                      fontSize: "12px",
+                      fontWeight: "500",
+                    }}
+                    className="card-text text-left difficulty"
+                  >
+                    Beginner
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-12 col-md-6 col-lg-3">
+          <div className="card course-card">
+            <div
+              style={{
+                backgroundColor: "#F1DED0",
+                padding: "1rem",
+                height: "60%",
+                display: "grid",
+                position: "relative",
+                zIndex: "0",
+                placeItems: "center",
+              }}
+            >
+              <div className="shapes">
+                <svg
+                  class="javascript-card2-shape1 img-fluid"
+                  width="100"
+                  viewBox="0 0 100 100"
+                  preserveAspectRatio="none"
+                  height="100"
+                >
+                  <path
+                    style={{ fill: "hsla(28.00,52.00%,95.00%,100%)" }}
+                    d="M46.6443 2.02753C66.6399 1.53324 88.5794 7.59278 95.422 28.5087C102.874 51.2865 93.5364 76.7516 75.5776 90.4677C58.9651 103.156 37.2191 98.7293 21.1053 85.2685C5.82684 72.5056 -2.06419 50.8861 4.10887 30.7659C10.0345 11.4522 28.3111 2.48073 46.6443 2.02753Z"
+                  ></path>
+                </svg>
+                <svg
+                  class="javascript-card2-shape2 img-fluid"
+                  width="100"
+                  viewBox="0 0 100 100"
+                  preserveAspectRatio="none"
+                  height="100"
+                >
+                  <path
+                    style={{ fill: "hsla(28.00,52.00%,95.00%,100%)" }}
+                    d="M46.6443 2.02753C66.6399 1.53324 88.5794 7.59278 95.422 28.5087C102.874 51.2865 93.5364 76.7516 75.5776 90.4677C58.9651 103.156 37.2191 98.7293 21.1053 85.2685C5.82684 72.5056 -2.06419 50.8861 4.10887 30.7659C10.0345 11.4522 28.3111 2.48073 46.6443 2.02753Z"
+                  ></path>
+                </svg>
+                <svg
+                  class="javascript-card2-shape3 img-fluid"
+                  width="100"
+                  viewBox="0 0 100 100"
+                  preserveAspectRatio="none"
+                  height="100"
+                >
+                  <path
+                    style={{ fill: "hsla(28.00,52.00%,95.00%,100%)" }}
+                    d="M46.6443 2.02753C66.6399 1.53324 88.5794 7.59278 95.422 28.5087C102.874 51.2865 93.5364 76.7516 75.5776 90.4677C58.9651 103.156 37.2191 98.7293 21.1053 85.2685C5.82684 72.5056 -2.06419 50.8861 4.10887 30.7659C10.0345 11.4522 28.3111 2.48073 46.6443 2.02753Z"
+                  ></path>
+                </svg>
+                <svg
+                  class="javascript-card2-shape4 img-fluid"
+                  width="100"
+                  viewBox="0 0 100 100"
+                  preserveAspectRatio="none"
+                  height="100"
+                >
+                  <path
+                    style={{ fill: "hsla(28.00,52.00%,95.00%,100%)" }}
+                    d="M46.6443 2.02753C66.6399 1.53324 88.5794 7.59278 95.422 28.5087C102.874 51.2865 93.5364 76.7516 75.5776 90.4677C58.9651 103.156 37.2191 98.7293 21.1053 85.2685C5.82684 72.5056 -2.06419 50.8861 4.10887 30.7659C10.0345 11.4522 28.3111 2.48073 46.6443 2.02753Z"
+                  ></path>
+                </svg>
+              </div>
+              <h2 className="text-right course-name">
+                The JavaScript Bootcamp
+              </h2>
+            </div>
+            <div className="card-body p-2">
+              <div className="row">
+                <div className="col-auto">
+                  <p
+                    className="card-text text-left"
+                    style={{
+                      fontSize: "12px",
+                      fontWeight: "500",
+                    }}
+                  >
+                    112 lessons | 13 hours
+                  </p>
+                  <p
+                    style={{
+                      fontSize: "12px",
+                      fontWeight: "500",
+                      marginTop: "-1rem",
+                    }}
+                    className="card-text text-left"
+                  >
+                    with <strong>Reed Barger</strong>
+                  </p>
+                </div>
+                <div className="col-auto ml-auto">
+                  <img
+                    src="https://scrimba.com/avatars/reedbarger/64"
+                    className="img-fluid avatar rounded-circle"
+                  />
+                </div>
+              </div>
+              <div className="row pt-5">
+                <div className="col">
+                  <p
+                    style={{
+                      fontSize: "12px",
+                      fontWeight: "500",
+                    }}
+                    className="card-text text-left difficulty"
+                  >
+                    Intermediate
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-12 col-md-6 col-lg-3">
+          <div className="card course-card">
+            <div
+              style={{
+                backgroundColor: "#D9CFFF",
+                padding: "1rem",
+                height: "60%",
+                display: "grid",
+                placeItems: "center",
+                position: "relative",
+                zIndex: "0",
+              }}
+            >
+              <div className="shapes">
+                <svg
+                  class="new-to-coding-card3-shape1 img-fluid"
+                  width="100"
+                  viewBox="0 0 100 100"
+                  preserveAspectRatio="none"
+                  height="100"
+                >
+                  <path
+                    style={{ fill: "white" }}
+                    d="M13.4722 14.9548C27.7804 -2.67845 49.7337 0.345336 67.4 8.41128C84.5927 16.261 100.585 31.1761 97.6499 53.388C94.6973 75.7326 75.1119 89.9688 55.8613 95.735C37.5404 101.223 18.5249 97.0492 9.47335 79.7998C-0.70101 60.4107 -1.5127 33.4221 13.4722 14.9548Z"
+                  ></path>
+                </svg>
+                <svg
+                  class="new-to-coding-card3-shape2 img-fluid"
+                  width="100"
+                  viewBox="0 0 100 100"
+                  preserveAspectRatio="none"
+                  height="100"
+                >
+                  <path
+                    style={{ fill: "white" }}
+                    d="M13.4722 14.9548C27.7804 -2.67845 49.7337 0.345336 67.4 8.41128C84.5927 16.261 100.585 31.1761 97.6499 53.388C94.6973 75.7326 75.1119 89.9688 55.8613 95.735C37.5404 101.223 18.5249 97.0492 9.47335 79.7998C-0.70101 60.4107 -1.5127 33.4221 13.4722 14.9548Z"
+                  ></path>
+                </svg>
+                <svg
+                  class="new-to-coding-card3-shape3 img-fluid"
+                  width="100"
+                  viewBox="0 0 100 100"
+                  preserveAspectRatio="none"
+                  height="100"
+                >
+                  <path
+                    style={{ fill: "white" }}
+                    d="M13.4722 14.9548C27.7804 -2.67845 49.7337 0.345336 67.4 8.41128C84.5927 16.261 100.585 31.1761 97.6499 53.388C94.6973 75.7326 75.1119 89.9688 55.8613 95.735C37.5404 101.223 18.5249 97.0492 9.47335 79.7998C-0.70101 60.4107 -1.5127 33.4221 13.4722 14.9548Z"
+                  ></path>
+                </svg>
+              </div>
+              <h2 className="text-right course-name">
+                Introduction to JavaScript
+              </h2>
+            </div>
+            <div className="card-body p-2">
+              <div className="row">
+                <div className="col-auto">
+                  <p
+                    className="card-text text-left"
+                    style={{
+                      fontSize: "12px",
+                      fontWeight: "500",
+                    }}
+                  >
+                    34 lessons | 1 hour
+                  </p>
+                  <p
+                    style={{
+                      fontSize: "12px",
+                      fontWeight: "500",
+                      marginTop: "-1rem",
+                    }}
+                    className="card-text text-left"
+                  >
+                    with <strong>Dylan C. Israel</strong>
+                  </p>
+                </div>
+                <div className="col-auto ml-auto">
+                  <img
+                    src="https://scrimba.com/avatars/PizzaPokerGuy/64"
+                    className="img-fluid avatar rounded-circle"
+                  />
+                </div>
+              </div>
+              <div className="row pt-5">
+                <div className="col">
+                  <p
+                    style={{
+                      fontSize: "12px",
+                      fontWeight: "500",
+                    }}
+                    className="card-text text-left difficulty"
+                  >
+                    Beginner
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-12 col-md-6 col-lg-3">
+          <div className="card course-card">
+            <div
+              style={{
+                backgroundColor: "#DCEAE0",
+                padding: "1rem",
+                height: "60%",
+                display: "grid",
+                placeItems: "center",
+                position: "relative",
+                zIndex: "0",
+              }}
+            >
+              <div className="shapes">
+                <svg
+                  class="javascript-card4-shape1 img-fluid"
+                  width="100"
+                  viewBox="0 0 100 100"
+                  preserveAspectRatio="none"
+                  height="100"
+                >
+                  <path
+                    style={{ fill: "hsla(252.00,64.76%,51.22%,100%)" }}
+                    d="M9.29261 5.23311C21.8211 -4.9955 35.5207 12.0092 49.7636 15.442C60.6519 18.0663 73.5169 14.3472 80.9487 22.5036C91.3853 33.9578 103.182 50.0749 95.5757 65.7208C88.0075 81.288 67.0064 78.5017 51.1777 83.7402C35.8226 88.8219 18.4865 104.422 6.87518 95.0759C-4.6975 85.7611 7.90853 65.3205 8.34065 49.2608C8.75633 33.8123 -3.15401 15.3948 9.29261 5.23311Z"
+                  ></path>
+                </svg>
+                <svg
+                  class="javascript-card4-shape2 img-fluid"
+                  width="100"
+                  viewBox="0 0 100 100"
+                  preserveAspectRatio="none"
+                  height="100"
+                >
+                  <path
+                    style={{ fill: "hsla(220.00,76.50%,65.82%,100%)" }}
+                    d="M6.95017 8.19765C17.2058 -2.99805 33.6242 4.33116 47.5944 4.64098C59.4603 4.90413 71.9293 2.79464 81.026 9.84042C91.1166 17.656 97.371 29.9185 97.9336 43.6309C98.542 58.4598 95.0477 74.8387 84.1413 85.7687C73.505 96.428 58.2977 96.6126 44.3219 97.3589C30.7609 98.083 15.181 100.035 6.7788 89.8996C-1.3716 80.068 5.49946 64.2539 5.52818 50.5633C5.55888 35.9261 -3.3047 19.3926 6.95017 8.19765Z"
+                  ></path>
+                </svg>
+              </div>
+              <h2 className="text-right course-name">
+                Build a Snake Game in JavaScript
+              </h2>
+            </div>
+            <div className="card-body p-2">
+              <div className="row">
+                <div className="col-auto">
+                  <p
+                    className="card-text text-left"
+                    style={{
+                      fontSize: "12px",
+                      fontWeight: "500",
+                    }}
+                  >
+                    28 lessons | 1 hours
+                  </p>
+                  <p
+                    style={{
+                      fontSize: "12px",
+                      fontWeight: "500",
+                      marginTop: "-1rem",
+                    }}
+                    className="card-text text-left"
+                  >
+                    with <strong>Ania Kubow</strong>
+                  </p>
+                </div>
+                <div className="col-auto ml-auto">
+                  <img
+                    src="https://scrimba.com/avatars/uid/uN32gmcm/64"
+                    className="img-fluid avatar rounded-circle"
+                  />
+                </div>
+              </div>
+              <div className="row pt-5">
+                <div className="col">
+                  <p
+                    style={{
+                      fontSize: "12px",
+                      fontWeight: "500",
+                    }}
+                    className="card-text text-left difficulty"
+                  >
+                    Intermediate
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-12">
+          <button
+            type="button"
+            class="see-all"
+            style={{
+              marginTop: "5rem",
+            }}
+          >
+            <strong>See All</strong>
+          </button>
         </div>
       </div>
     </div>
